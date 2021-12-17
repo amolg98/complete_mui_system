79a79ee37db577fc8f0444b5588a8c5aa3c11bbf
agupta <amol.gupta@us.sios.com>
2021-12-16 23:34:33 -0500

    [P-001] Second tutorial, third started

--- complete_mui_system/src/App/App.js
+++ complete_mui_system/src/App/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import './App.css';
 import SideMenu from '../components/SideMenu';
-import { makeStyles, CssBaseline } from '@material-ui/core';
+import { makeStyles, CssBaseline, createTheme, ThemeProvider } from '@material-ui/core';
 import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+ 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526"
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  // shape: {
+  //   borderRadius: '12px',
+  // },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)'
+      }
+    }
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -15,13 +48,18 @@ function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader 
+          title = "Page Header"
+          subTitle = "Page Description"
+          icon = {<PeopleOutlineTwoToneIcon fontSize='large'/>}
+        />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
